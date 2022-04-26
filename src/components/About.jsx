f8397ba3a47315af444a16cca357efb9f5936c50
GorMkrtchyan <gormkrtchyann@gmail.com>
2022-04-26 22:47:18 +0400

    fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 const About = () =>{
     return(
@@ -34,29 +34,32 @@ const About = () =>{
                 </div>
             </div>
 
-            <Carousel className="about_mobile">
-                <Carousel.Item className="block block-1">
-                    <img src="images/vectors/seo.png"/>
-                    <div className="block_text">
-                        <h2>Վեբ Ծրագարվորում</h2>
-                        <p>Սովորեք վեբ ծրագրավորում մեզ հետ</p>
-                    </div>
-                </Carousel.Item>
-                <Carousel.Item className="block block-2">
-                    <img src="images/vectors/web-developer.png"/>
-                    <div className="block_text">
-                        <h2>Զարգացնող Խաղեր</h2>
-                        <p>Զարգացրեք ծրագրավորման գիտելիքները խաղեր խաղալով</p>
-                    </div>
-                </Carousel.Item>
-                <Carousel.Item className="block block-3">
-                    <img src="images/vectors/website-recovery.png"/>
-                    <div className="block_text">
-                        <h2>Պատրաստի Կայքերի Թեմպլեյտներ</h2>
-                        <p>Կարող ես այստեղից ներբեռնել պատրաստի կայքեր և ձևափոխելով այն ստանալ ձեր ուզածը</p>
-                    </div>
-                </Carousel.Item>
-            </Carousel>
+
+            <div className="about_mobile">
+                <Carousel>
+                    <Carousel.Item className="block block-1">
+                        <img src="images/vectors/seo.png"/>
+                        <div className="block_text">
+                            <h2><b>Վ</b>եբ <b>Ծ</b>րագարվորում</h2>
+                            <p>Սովորեք վեբ ծրագրավորում մեզ հետ</p>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item className="block block-2">
+                        <img src="images/vectors/web-developer.png"/>
+                        <div className="block_text">
+                            <h2><b>Զ</b>արգացնող <b>Խ</b>աղեր</h2>
+                            <p>Զարգացրեք ծրագրավորման գիտելիքները խաղեր խաղալով</p>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item className="block block-3">
+                        <img src="images/vectors/website-recovery.png"/>
+                        <div className="block_text">
+                            <h2><b>Պ</b>ատրաստի <b>Կ</b>այքերի <b>Թ</b>եմպլեյտներ</h2>
+                            <p>Կարող ես այստեղից ներբեռնել պատրաստի կայքեր և ձևափոխելով այն ստանալ ձեր ուզածը</p>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         </section>
     )
 }
